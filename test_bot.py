e4e5fced2dec200d2a68adaf03b3b1662a2b1b0a
Shekspii <zlozt403@gmail.com>
2025-03-26 19:47:01 +0700

    То же самое, что и предыдущий commit, но оно работает. +Перевёл цикл с проверкой уведомлений в фон.

--- test_bot.py
+++ test_bot.py
@@ -1,4 +1,4 @@
-import telebot, asyncio, time
+import telebot, threading, time
 from telebot import types
 from datetime import datetime, date, timedelta
 
@@ -22,8 +22,8 @@ class Event:
 
     def Status(self):
         today = datetime.today() #Текущая дата и время  
-        return self.create_event_datetime() > today # Если Дата события не наступила, то оно доступно. Эта строка кидает False/True. Потом эту строку
-                                       # в аргумент берёт на 14 строке в статус(False/True)
+        return self.create_event_datetime() > today # Если Дата события не наступила, то оно доступно. Эта строка кидает False/True. Потом эту 
+                                       # строку в аргумент берёт на 14 строке в статус(False/True)
 
     def __str__(self):
         """Вывод информации о мероприятии"""
@@ -66,60 +66,58 @@ class EventBot:
             Event(1002, 'Праздник 9 мая', '11:54', '2025-05-09', 'RF'),
             Event(1003, 'Праздник 24 февраля', '22:40', '2025-02-24', 'USSR'),
             Event(1004, 'Не Масленница', '23:22', '2025-02-27', 'Rus,'),
+            Event(1005, 'Test_Notifs', '20:20', '2025-03-26', 'Me'),
         ]
-        
+        self.setup_handlers()
+
+    def setup_handlers(self):
+        """Настройка команд и кнопок"""
+
         @self.bot.message_handler(commands=['start', 'help'])
         def start_message(message):
-            now = datetime.now().hour # который час?
+            now = datetime.now().hour
             if 6 <= now < 12:
                 day_period = "Доброе утро"
             elif 12 <= now < 23:
                 day_period = "Добрый день"
             else:
                 day_period = "Доброй ночи"
-            self.bot.send_message(message.chat.id,{day_period}) 
-            sent_notifications = set()
-            print(sent_notifications)
-            while True:
-                if self.events:
-                    i = 0
-                    for event in self.events:
-                        eventik = self.events[i]      
-                        self.notif_obj = Notification(eventik)
-                        notif_text = str(self.notif_obj)  # Получаем текст уведомления
-                        if notif_text and event.event_name not in sent_notifications:  # Отправляем, только если строка не пустая
-                            self.bot.send_message(message.chat.id, notif_text)
-                            sent_notifications.add(event.event_name)
-                        i+=1
-                    time.sleep(30)
-                else:
-                    self.bot.send_message(message.chat.id, 'Нет доступных событий')        
+            self.bot.send_message(message.chat.id, day_period)
+
+            # Запуск уведомлений в отдельном потоке
+            threading.Thread(target=self.send_notifications, args=(message.chat.id,), daemon=True).start()
 
         @self.bot.message_handler(commands=['button'])
         def button_message(message):
-            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1=types.KeyboardButton("Мероприятия")
+            #print("Команда /button получена!")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1 = types.KeyboardButton("Мероприятия")
+            item2 = types.KeyboardButton("Профиль")
+            markup.add(item1, item2)
+            self.bot.send_message(message.chat.id, 'Выберите, что вам надо', reply_markup=markup)
 
-            item3=types.KeyboardButton("Профиль")
-            markup.add(item1,  item3)
-            self.bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
-
-        @self.bot.message_handler(content_types='text')
+        @self.bot.message_handler(func=lambda message: True)
         def message_reply(message):
             if message.text == "Мероприятия":
-                show_events = "\n".join(str(event)for event in self.events) if self.events else "Нет доступных мероприятий"
+                show_events = "\n".join(str(event) for event in self.events) if self.events else "Нет доступных мероприятий"
+
                 self.bot.send_message(message.chat.id, show_events)
-            
-            elif message.text == "Назад":
-                markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-                item1=types.KeyboardButton("Мероприятия")
-                
-                item3=types.KeyboardButton("Профиль")
-                markup.add(item1,  item3)
-                self.bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
             elif message.text == "Профиль":
-                show_usr = str(User)
-                self.bot.send_message(message.chat.id, show_usr)
+                usr_obj = User(message)
+                show_user = str(usr_obj)
+                self.bot.send_message(message.chat.id, show_user)
+
+    def send_notifications(self, chat_id):
+        """Фоновая проверка уведомлений"""
+        sent_notifications = set()
+        while True:
+            for event in self.events:
+                notif_obj = Notification(event)
+                notif_text = str(notif_obj)
+                if notif_text and event.event_name not in sent_notifications:
+                    self.bot.send_message(chat_id, notif_text)
+                    sent_notifications.add(event.event_name)
+            time.sleep(60)
             
     def run(self):
         """Метод для запуска бота"""
